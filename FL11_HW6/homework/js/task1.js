d63b6f562a689e06d53f40723f00a0ed90472e63
Valentine Karnaukhov <51517350+random1zer@users.noreply.github.com>
2019-07-07 13:56:16 +0300

    fixed

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,1 +1,19 @@
-// Your code goes here
+const a1 = prompt('Enter x for point A');
+const a2 = prompt('Enter y for point A');
+const b1 = prompt('Enter x for point B');
+const b2 = prompt('Enter y for point B');
+const c1 = prompt('Enter x for point C');
+const c2 = prompt('Enter y for point C');
+
+const mid_x = (a1 + b1) / 2;
+const mid_y = (a2 + b2) / 2;
+
+if (mid_x === c1 && mid_y === c2) {
+  console.log(true);
+ }else {
+  console.log(false);
+}
+
+
+
+
--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,1 +1,18 @@
-// Your code goes here
+const a = prompt('Enter a= ');
+const b = prompt('Enter b= ');
+const c = prompt('Enter c= ');
+
+if (a + b >= c && a + c >= b && b + c >= a) {
+    if (a === b && a === c && b === c) {
+        console.log('Eequivalent triangle');
+
+    } else if (a === b || a === c || b === c) {
+        console.log('Isosceles triangle');
+
+    } else {
+        console.log('Normal triangle');
+    }
+
+} else {
+    console.log('Triangle doesn’t exist');
+}
